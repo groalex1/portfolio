97bea6b60718eeb2a2dd87fff56c8609d743fc76
groalex1 <gromanalexander@gmail.com>
2025-02-11 20:37:16 -0500

    enabled analytics using vercel analytics import

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,14 @@
 import "./globals.css";
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Analytics } from "@vercel/analytics/react"
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         {children}
+        <Analytics />
       </body>
     </html>
   );
