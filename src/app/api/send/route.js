8fe6f69fbba0ba9b6e848b873f87f6dbc2911a57
groalex1 <gromanalexander@gmail.com>
2025-04-23 22:40:39 -0400

    email change

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,14 +1,15 @@
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+resend.domains.create({ name: 'alexanderdev.info' });
 
 export async function POST(request) {
   try {
     const { email, subject, message } = await request.json();
 
     const { data, error } = await resend.emails.send({
-      from: 'Alexander <alexanderdev1912@gmail.com>', // Your verified domain email
-      to: ['your-email@example.com'], // Your receiving email
+      from: 'Alexander <alexanderdev1912@gmail.com>',
+      to: ['gromanalexander@gmail.com'], 
       subject: subject,
       html: `
         <p>New message from: ${email}</p>
